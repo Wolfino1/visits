5153fdca475cdfe6788d56bcf3eda2f115469271
Santiago <sanguefer@gmail.com>
2025-04-23 20:05:39 -0500

    Visits project

--- src/main/java/com/visitas/visitas/visitas/infrastructure/endpoints/rest/VisitsController.java
+++ src/main/java/com/visitas/visitas/visitas/infrastructure/endpoints/rest/VisitsController.java
@@ -1,5 +1,6 @@
 package com.visitas.visitas.visitas.infrastructure.endpoints.rest;
 
+
 import com.visitas.visitas.visitas.application.dto.request.SaveVisitsRequest;
 import com.visitas.visitas.visitas.application.dto.response.VisitsResponse;
 import com.visitas.visitas.visitas.application.mappers.VisitsDtoMapper;
